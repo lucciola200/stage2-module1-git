1efb1f3ed4e8f39b2ec2bf7259fe0056b7b9283c
lucciolla200 <teoteo.odisharia@gmail.com>
2023-11-28 17:01:56 +0400

    solution

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/lucciola200/mjc-git-task";
+    public static String REPO_LINK = "https://github.com/lucciola200/mjc-git-task.git";
 }
